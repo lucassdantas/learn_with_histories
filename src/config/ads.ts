1ceb05d425f6df0779f1810fe8b812962cf26d1d
lucas <lucasdantasprogramador@gmail.com>
2026-05-07 01:52:25 -0300

    FEAT: add ads.txt

--- src/config/ads.ts
+++ src/config/ads.ts
@@ -1,12 +1,17 @@
 export const ADS_CONFIG = {
   // Use Google's test publisher ID
   //publisherId: 'ca-pub-3940256099942544',
+  // slots: {
+  //   banner: '6300978111',
+  //   popup: '1033173712',
+  //   sidebar: '6300978111', // Reusing banner for side if needed
+  // },
+
   publisherId: 'ca-pub-3124364125222536',
   slots: {
-    banner: '6300978111',
-    popup: '1033173712',
-    sidebar: '6300978111', // Reusing banner for side if needed
+    banner: '1744357937',
+    popup: '3057439605',
+    sidebar: '5129069170',
   },
-  // To change to real ads, just replace the IDs above
-  isTest: true,
+  isTest: false,
 };
